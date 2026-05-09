103947c5f4c6d1b58e9a870edad9dfdb3cd58500
VoyagerZhao <3403653253@qq.com>
2026-05-09 22:33:35 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# zc-ai-agent
---
+++ src/test/java/com/nozc/zcaiagent/ZcAiAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nozc.zcaiagent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZcAiAgentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
